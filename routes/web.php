378c56715549dc94f7f2e2f054ddb0e6b1440d4b
EnricoMoses <enricomoseswijaya@gmail.com>
2026-02-27 21:06:39 +0700

    final project

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\InventoryController;
 
+Route::redirect('/', '/inventory');
 
-Route::prefix('tugas')
-    ->name('members.')
-    ->controller(MemberController::class)
+Route::prefix('inventory')
+    ->name('inventory.')
+    ->controller(InventoryController::class)
     ->group(function () {
 
         Route::get('/', 'index')->name('index');
         Route::post('/', 'store')->name('store');
-        Route::get('/view/{id}', 'view')->name('view');
         Route::get('/edit/{id}', 'edit')->name('edit');
         Route::put('/{id}', 'update')->name('update');
         Route::delete('/{id}', 'destroy')->name('destroy');
     });
-
-
-Route::get('/', [InventoryController::class, 'index'])->name('inventory.index');
-Route::post('/', [InventoryController::class, 'store'])->name('inventory.store');
-Route::get('/edit/{id}', [InventoryController::class, 'edit'])->name('inventory.edit');
-Route::put('/{id}', [InventoryController::class, 'update'])->name('inventory.update');
-Route::delete('/{id}', [InventoryController::class, 'destroy'])->name('inventory.destroy');
 
 
